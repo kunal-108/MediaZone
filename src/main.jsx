b7aa70a85211793f33604d8bb0a6a68b73323203
kunnu108 <kunal.mahor.108@gmail.com>
2026-04-16 00:44:01 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <Provider store={store}>
-            <BrowserRouter basename="/MediaZone">
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </StrictMode>,
 );
